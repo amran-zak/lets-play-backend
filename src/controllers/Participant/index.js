5b3d07413a88bc5f26990ccddd82bc8ac1464137
perrineosw <oswald.perrinedu40@gmail.com>
2024-01-04 23:15:47 +0100

    fix

--- src/controllers/Participant/index.js
+++ src/controllers/Participant/index.js
@@ -65,16 +65,16 @@ exports.isParticipatingInSport = async (req, res) => {
     const sportId = req.params.sportId;
     const userId = req.user._id;
 
-    // Vérifier si l'utilisateur participe au sport
-    const participation = await Participation.findOne({ participant: userId, sport: sportId }).populate('sport');
-    
-    // Si la participation n'est pas trouvée, l'utilisateur ne participe pas
-    if (!participation) {
-      return res.status(200).send({ isParticipating: false });
-    }
+    // Vérifier si l'utilisateur est l'organisateur du sport
+    const isOrganizer = await Sport.findOne({ _id: sportId, organizer: userId });
 
-    // Si la participation est trouvée, l'utilisateur participe
-    return res.status(200).send({ isParticipating: true, sport: participation.sport });
+    // Vérifier si l'utilisateur participe au sport
+    const isParticipant = await Participation.findOne({ participant: userId, sport: sportId });
+
+    // L'utilisateur participe s'il est participant ou organisateur
+    const isParticipating = !!isOrganizer || !!isParticipant;
+
+    return res.status(200).send({ isParticipating });
   } catch (err) {
     return res.status(500).send({ message: "Erreur serveur", error: err.message });
   }
